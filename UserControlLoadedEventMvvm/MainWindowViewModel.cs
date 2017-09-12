ddd9f52c3c2cefe44deba5e34da47698d720a43a
Mallikarjuna Kalkere <mkalkere@gmail.com>
2017-09-12 12:56:10 -0400

    Handling Events in MVVM

---
+++ UserControlLoadedEventMvvm/MainWindowViewModel.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Input;
+
+namespace UserControlLoadedEventMvvm
+{
+    public class MainWindowViewModel
+    {
+
+        #region Constructor
+        public MainWindowViewModel()
+        {
+            InitializeCommands();
+        }
+        #endregion
+
+        #region Commands
+        public ICommand LoadedCommand { get; set; }
+
+        private void InitializeCommands()
+        {
+            LoadedCommand = new CustomCommand(OnUserControlLoaded, CanUserControlLoadedExecute);
+
+        }
+
+        private bool CanUserControlLoadedExecute(object obj)
+        {
+            return true;
+        }
+
+        private void OnUserControlLoaded(object obj)
+        {
+            MessageBox.Show("User Control Loaded!", "Event Fired!!!", MessageBoxButton.OK);
+        }
+        #endregion
+
+    }
+}
